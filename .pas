9a8923c576c7c5a8043c04f0bc806330bb5a8e50
jose chacon <j.chacon1443@unimar.edu.ve>
2023-10-26 23:47:08 -0400

    inicio

---
+++ .pas
@@ -0,0 +1,19 @@
+
+program nn;
+
+uses crt;
+var oction:integer;
+
+BEGIN
+repeat 
+	writeln ('hola');
+	readln (oction);
+	until oction<> 2;
+	case oction of 
+	
+	1:begin
+writeln('hola');
+end;
+	end;
+END.
+
